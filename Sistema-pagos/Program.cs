39faeec8a5210e6d7ab727fdbf682009ce1557c0
Fede Petta <federicopetta234@gmail.com>
2026-06-10 16:41:30 -0300

    Menu de opciones agregado

---
+++ Sistema-pagos/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int op;
+        do
+        {
+            Console.WriteLine("--Menu--");
+            Console.WriteLine("1. Ingresar datos");
+            Console.WriteLine("2. Mostrar datos");
+            Console.WriteLine("0. Salir");
+            Console.Write("Seleccione una opción: ");
+            op = int.Parse(Console.ReadLine());
+
+            switch (op)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 0:
+                    Console.WriteLine("Saliendo...");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Intente nuevamente.");
+                    break;
+            }
+        }while (op != 0);
+    }
+}
